f46640b07f58ccd0c9e2ab724ca09d88bb2637e9
Moses L. Jilugu <mosesjil@ie.ibm.com>
2016-03-10 14:34:47 +0000

    Update java inclass documentation.

--- suncertify/mvn_build/src/main/java/suncertify/App.java
+++ suncertify/mvn_build/src/main/java/suncertify/App.java
@@ -16,6 +16,7 @@ import java.awt.event.WindowEvent;
  * methods to get user settings, initialize database parameters from
  * properties file and to launch the application whether in single mode or client only mode
  * or to launch the network server mode.
+ * 
  * @author Moses L. Jilugu
  * @version Dec 11, 2012
  */
